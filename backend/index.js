b8a14440ba974493f4563d588cdfbe5db3c2fed1
muchfahmiarif <fahmiarif96@gmail.com>
2023-04-18 02:07:06 +0700

    `backend` add model Users and import into index.js for testing

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import db from "./config/Database.js";
+import Users from "./models/UserModel.js";
+
 const app = express();
 
 try {
   await db.authenticate();
   console.log("Database connected");
-} catch {
+  await Users.sync(); // jika tidak ada tabel, maka akan dibuatkan tabel baru
+} catch (error) {
   console.error(error);
 }
 
