86dababd0f547c8991d883996399378e1411da73
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-13 06:32:10 +0700

    `frontend` add register and add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/register" element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-4-desktop">
+                <form action="" className="box">
+                  <div className="field mt-5">
+                    <label htmlFor="" className="label">
+                      Name
+                    </label>
+                    <div className="controls">
+                      <input type="text" className="input" placeholder="Name" />
+                    </div>
+                  </div>
+                  <div className="field mt-5">
+                    <label htmlFor="" className="label">
+                      Email
+                    </label>
+                    <div className="controls">
+                      <input type="text" className="input" placeholder="Email" />
+                    </div>
+                  </div>
+                  <div className="field mt-5">
+                    <label htmlFor="" className="label">
+                      Password
+                    </label>
+                    <div className="controls">
+                      <input type="password" className="input" placeholder="******" />
+                    </div>
+                  </div>
+                  <div className="field mt-5">
+                    <label htmlFor="" className="label">
+                      Confirm Password
+                    </label>
+                    <div className="controls">
+                      <input type="password" className="input" placeholder="******" />
+                    </div>
+                  </div>
+                  <div className="field mt-5">
+                    <button className="button is-success is-fullwidth">Register</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Register;
