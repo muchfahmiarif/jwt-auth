d3bef753666bf02593833f4f81e11259283abca3
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-13 07:51:34 +0700

    `frontend` add dashboard, navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -9,6 +11,14 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login />}></Route>
         <Route exact path="/register" element={<Register />}></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <Navbar />
+              <Dashboard />
+            </>
+          }></Route>
       </Routes>
     </BrowserRouter>
   );
