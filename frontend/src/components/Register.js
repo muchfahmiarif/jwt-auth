1c2c1ef3d05200e2f6ea664b7853644ba8dcf2d2
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-15 21:05:06 +0700

    `frontend` test register when password not same will appear message on console

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passowrd, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const Register = async (e) => {
+    e.preventDefault(); // while on submit page will not refresh
+    try {
+      await axios.post(`http://localhost:5000/users`, {
+        name: name,
+        email: email,
+        password: passowrd,
+        confirmPassword: confirmPassword,
+      });
+      navigate("/");
+    } catch (error) {
+      if (error.response) {
+        // setMsg(error.response.data.message);
+        console.log(error.response.data.message); // message from backend
+      }
+    }
+  };
+
   return (
     <div>
       <section className="hero has-background-grey-light is-fullheight is-fullwidth">
@@ -8,13 +36,13 @@ const Register = () => {
           <div className="container">
             <div className="columns is-centered">
               <div className="column is-4-desktop">
-                <form action="" className="box">
+                <form action="" onSubmit={Register} className="box">
                   <div className="field mt-5">
                     <label htmlFor="" className="label">
                       Name
                     </label>
                     <div className="controls">
-                      <input type="text" className="input" placeholder="Name" />
+                      <input type="text" className="input" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
                     </div>
                   </div>
                   <div className="field mt-5">
@@ -22,7 +50,7 @@ const Register = () => {
                       Email
                     </label>
                     <div className="controls">
-                      <input type="text" className="input" placeholder="Email" />
+                      <input type="text" className="input" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                   </div>
                   <div className="field mt-5">
@@ -30,7 +58,7 @@ const Register = () => {
                       Password
                     </label>
                     <div className="controls">
-                      <input type="password" className="input" placeholder="******" />
+                      <input type="password" className="input" placeholder="******" value={passowrd} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                   </div>
                   <div className="field mt-5">
@@ -38,7 +66,13 @@ const Register = () => {
                       Confirm Password
                     </label>
                     <div className="controls">
-                      <input type="password" className="input" placeholder="******" />
+                      <input
+                        type="password"
+                        className="input"
+                        placeholder="******"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                      />
                     </div>
                   </div>
                   <div className="field mt-5">
