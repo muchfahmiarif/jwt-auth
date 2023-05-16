b2265ab259f6e68da1d4988a4a3aae7194530526
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-16 23:17:04 +0700

    `frontend` create refresh token and testing on console

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 const Dashboard = () => {
+  const [name, setName] = useState("");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    refreshToken();
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/token");
+      setToken(response.data.accessToken);
+      const decoded = jwt_decode(response.data.accessToken);
+      console.log(decoded);
+    } catch (error) {}
+  };
+
   return (
     <div className="container mt-5">
       <h1>Welcome Back : </h1>
